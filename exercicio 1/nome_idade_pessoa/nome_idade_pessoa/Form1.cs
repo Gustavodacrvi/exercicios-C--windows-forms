1bac0b2c36948330aa70ab21d0c11b2b43d3306e
Gustavo da Cruz Vieira <45800674+Gustavodacrvi@users.noreply.github.com>
2019-02-18 14:14:00 -0300

    form layout

--- exercicio 1/nome_idade_pessoa/nome_idade_pessoa/Form1.cs
+++ exercicio 1/nome_idade_pessoa/nome_idade_pessoa/Form1.cs
@@ -16,5 +16,50 @@ namespace nome_idade_pessoa
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
