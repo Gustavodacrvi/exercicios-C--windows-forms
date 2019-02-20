8160d7e615d69587ebeae66792e6bce4cee3e546
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-02-20 20:35:20 -0300

    exercicio pronto

--- exercicio 3/consumo_medio_automovel/consumo_medio_automovel/Form1.cs
+++ exercicio 3/consumo_medio_automovel/consumo_medio_automovel/Form1.cs
@@ -16,5 +16,27 @@ namespace consumo_medio_automovel
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        public void calculate()
+        {
+            int num1;
+            int num2;
+
+            if (Int32.TryParse(textBox1.Text, out num1) && Int32.TryParse(textBox2.Text, out num2))
+            {
+                label5.Text = Convert.ToString(num1 / num2);
+                label5.Text += " metros/litro";
+            }
+        }
     }
 }
