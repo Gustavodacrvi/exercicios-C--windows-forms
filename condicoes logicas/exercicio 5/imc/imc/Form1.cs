57189ef529f7bc765e200dd4fcf042496bcb70f4
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-12 18:14:00 -0300

    exercicio 5 codigo

--- condicoes logicas/exercicio 5/imc/imc/Form1.cs
+++ condicoes logicas/exercicio 5/imc/imc/Form1.cs
@@ -16,5 +16,41 @@ namespace imc
         {
             InitializeComponent();
         }
+
+        public void calcular()
+        {
+            double valor1, valor2, imc;
+            if (Double.TryParse(peso.Text, out valor1) && Double.TryParse(altura.Text, out valor2))
+            {
+                imc = (valor2 * valor2) / valor1;
+                IMC.Text = Convert.ToString(imc);
+                if (imc < 18.5)
+                {
+                    resultado.Text = "MAGRO";
+                }
+                else if (imc >= 18.5 && imc < 25)
+                {
+                    resultado.Text = "NORMAL";
+                }
+                else if (imc >= 25 && imc < 30)
+                {
+                    resultado.Text = "OBESO";
+                }
+                else
+                {
+                    resultado.Text = "OBESO MORBIDO";
+                }
+            }
+        }
+
+        private void altura_TextChanged(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        private void peso_TextChanged(object sender, EventArgs e)
+        {
+            calcular();
+        }
     }
 }
