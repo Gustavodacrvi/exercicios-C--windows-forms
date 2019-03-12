ab7a9b0d2e9b9da94fbcd6a5ebd1cfe92481a539
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-12 17:26:56 -0300

    exercicio 2 codigo

--- condicoes logicas/exercicio 2/tres_numeros_inteiros/tres_numeros_inteiros/Form1.cs
+++ condicoes logicas/exercicio 2/tres_numeros_inteiros/tres_numeros_inteiros/Form1.cs
@@ -16,5 +16,42 @@ namespace tres_numeros_inteiros
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        public void calcular()
+        {
+            int[] valores = Enumerable.Range(0, 3).ToArray();
+            if (Int32.TryParse(textBox1.Text, out valores[0]) && Int32.TryParse(textBox2.Text, out valores[1]) && Int32.TryParse(textBox3.Text, out valores[2]))
+            {
+                int maior = valores[0];
+                for (int i =1;i<valores.Length;i++)
+                {
+                    if (valores[i] > maior)
+                    {
+                        maior = valores[i];
+                    }
+                }
+                resultado.Text = Convert.ToString(maior);
+            }
+        }
     }
 }
