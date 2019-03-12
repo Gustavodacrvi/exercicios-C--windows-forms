8a44d42c9f80e1a0cf487a65a484e3fd817912dd
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-12 17:47:17 -0300

    exercicio 4 layout

---
+++ condicoes logicas/exercicio 4/nota_media_aluno/nota_media_aluno/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace nota_media_aluno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.nota1 = new System.Windows.Forms.TextBox();
+            this.nota2 = new System.Windows.Forms.TextBox();
+            this.nota3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOTA MÉDIA DO ALUNO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nota 3:";
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(78, 79);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(213, 20);
+            this.nome.TabIndex = 5;
+            // 
+            // nota1
+            // 
+            this.nota1.Location = new System.Drawing.Point(78, 109);
+            this.nota1.Name = "nota1";
+            this.nota1.Size = new System.Drawing.Size(213, 20);
+            this.nota1.TabIndex = 6;
+            // 
+            // nota2
+            // 
+            this.nota2.Location = new System.Drawing.Point(78, 140);
+            this.nota2.Name = "nota2";
+            this.nota2.Size = new System.Drawing.Size(213, 20);
+            this.nota2.TabIndex = 7;
+            // 
+            // nota3
+            // 
+            this.nota3.Location = new System.Drawing.Point(78, 170);
+            this.nota3.Name = "nota3";
+            this.nota3.Size = new System.Drawing.Size(213, 20);
+            this.nota3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 321);
+            this.Controls.Add(this.nota3);
+            this.Controls.Add(this.nota2);
+            this.Controls.Add(this.nota1);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox nota1;
+        private System.Windows.Forms.TextBox nota2;
+        private System.Windows.Forms.TextBox nota3;
+    }
+}
+
